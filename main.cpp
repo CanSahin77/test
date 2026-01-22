54b7aeb955b161aa78f53c18662bb44f8aab886e
CanSahin77 <cannsahiinn@gmail.com>
2026-01-22 22:59:45 +0300

    benim

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <SFML/Graphics.hpp>
+enum class GameState{
+    Pause,
+    Play
+};
+class Menu{
+    public:
+        void toggle(GameState& state){
+            if(state == GameState::Play){
+                state = GameState::Pause;
+            }else{
+                state = GameState::Play;
+            }
+        };
 
+};
 int main(){
-    sf::RenderWindow window(sf::VideoMode({800, 600}),"DENEME");
+    GameState state = GameState::Pause;
+    Menu menu;
+    sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "DENEME", sf::Style::None);
     while(window.isOpen()){
         while(auto event = window.pollEvent()){
             if(event->is<sf::Event::Closed>()){
                 window.close();
-            }
+            };
+            if(auto key = event->getIf<sf::Event::KeyPressed>()){
+                if(key->code == sf::Keyboard::Key::Escape){
+                    menu.toggle(state);
+                }
+            };
         }
         window.clear(sf::Color(64, 64, 64));
         window.display(); 
